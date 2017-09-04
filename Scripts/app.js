6388fc2bacfc1ced59545f035ba359eeab1e9006
waqaramjad <waqaramjad345@gmail.com>
2017-09-04 22:17:18 +0500

    todo ap without data base

---
+++ Scripts/app.js
@@ -0,0 +1,91 @@
+var inputText = document.getElementById('inputText');
+var orderList = document.getElementById('list');
+var btn = document.getElementById('buttonAdd');
+var tempDatastorage ;
+
+function addTodoItem()
+{
+    var textValue = inputText.value;
+if(textValue=== '')
+{
+    alert('enter something in the box')
+}
+else
+{
+    var textNode = document.createTextNode(textValue);
+    var ListItem = document.createElement('li');
+    
+    var deletebutton = document.createElement('img');
+    deletebutton.src = 'images/delete.png';
+    deletebutton.style.marginLeft = '7px';
+    deletebutton.title = 'Delete';
+    deletebutton.setAttribute('onclick','deleteNode(this.parentNode)')
+    deletebutton.setAttribute('class','btnFloat')
+    var editButton = document.createElement('img');
+    editButton.src = 'images/edit.png';
+    editButton.title = 'Edit';
+    editButton.setAttribute('onclick','editNode(this.parentNode)');
+    editButton.setAttribute('class','btnFloat')
+    ListItem.appendChild(textNode); 
+    ListItem.appendChild(deletebutton);
+    ListItem.appendChild(editButton)
+    orderList.appendChild(ListItem);
+    inputText.value = '';}
+   
+
+}
+
+function deleteNode(getlist)
+{
+    orderList.removeChild(getlist);
+
+}
+function editNode(parentText)
+{
+    tempDatastorage = parentText;
+    age = parentText;
+    console.log(parentText);
+    inputText.value = parentText.childNodes[0].nodeValue;
+    btn.src = 'images/tick.png';
+    btn.title =  'save';
+    btn.setAttribute('onclick','saveNode( inputText.value)')
+
+
+
+}
+
+function saveNode( updatedTextValue)
+{
+    tempDatastorage.childNodes[0].nodeValue = updatedTextValue; 
+     btn.src = 'images/add.png';
+      btn.title = 'Add'
+     btn.setAttribute('onClick',"addTodoItem('inputText');");
+     inputText.value = '';
+
+}
+
+
+var myIndex = 0;
+        carousel();
+
+        function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 900);    
+}
+
+function textStyling()
+{
+    inputText.style.backgroundColor = '#39CCCC';
+}
+
+function textStylingRemove()
+{
+    inputText.style = 'reset';
+}
